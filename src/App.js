a9d7a41d20121425816415e79d702f388492c8f9
krutarthlad <krutarthlad321@gmail.com>
2024-07-14 10:46:04 -0500

    Second commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import  "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AddUser from './Users/AddUser';
+import EditUser from './Users/EditUser';
+import ViewUser from './Users/ViewUser';
+
+// new commit 2024
+function App() {
+  return (
+    <div className="App">
+
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/adduser' element= {<AddUser/>}/>
+        <Route exact path = '/edituser/:id' element ={<EditUser/>}/>
+        <Route exact path = '/viewuser' element ={<ViewUser/>}/>
+      </Routes>
+       </Router>
+
+      
+    </div>
+  );
+}
+
+export default App;
